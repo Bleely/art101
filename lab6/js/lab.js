718fa170db3732d1300f17105b3f3c225f24301d
Bleely <166079064+Bleely@users.noreply.github.com>
2024-04-24 17:01:37 -0700

    Lab 6 completed

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6
+// Author: Bailey Brothers <bajbroth@ucsc.edu
+// Date: 29 April 2024
 
 // Constants
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Define Variables 
+myTransport = ["Ford Taurus", " Bus", " Bike", " B-Cycle."];
+
+// Create an object for my main ride
+myMainRide = {
+  make: "Ford",
+  model: "Taurus",
+  color: "rusty",
+  year: 1995,
+  age: function() {
+      return 2024 - this.year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Output
+document.writeln("Ways I get around: ", myTransport, "</br>");
+document.writeln("My main ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
